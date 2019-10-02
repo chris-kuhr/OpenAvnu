9f628ed5896be4471b8e82d1c03ce502d95b116d
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-10-02 21:18:32 +0200

    console arguments s and e used for stream id and mac address

--- examples/jackd-talker/jackd_talker.c
+++ examples/jackd-talker/jackd_talker.c
@@ -310,10 +310,10 @@ int main(int argc, char *argv[])
 	memcpy(ctx->monitor_stream_id, glob_stream_id, sizeof(glob_stream_id));
 
 	printf("Stream ID: %02x%02x%02x%02x%02x%02x%02x%02x",
-                                     ctx->stream_id[0], ctx->stream_id[1],
-                                     ctx->stream_id[2], ctx->stream_id[3],
-                                     ctx->stream_id[4], ctx->stream_id[5],
-                                     ctx->stream_id[6], ctx->stream_id[7]);
+                                     ctx->monitor_stream_id[0], ctx->monitor_stream_id[1],
+                                     ctx->monitor_stream_id[2], ctx->monitor_stream_id[3],
+                                     ctx->monitor_stream_id[4], ctx->monitor_stream_id[5],
+                                     ctx->monitor_stream_id[6], ctx->monitor_stream_id[7]);
 
 
 	rc = mrp_connect(ctx);
