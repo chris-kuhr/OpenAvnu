d40087002bfec6b4cb59534799a4c8a27ed57dd8
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-12-14 11:05:29 +0100

    addd mcl kern.c

--- examples/jackd-listener/xdp_avb_MCL_kern.c
+++ examples/jackd-listener/xdp_avb_MCL_kern.c
@@ -76,7 +76,7 @@ int  xdp_avtp_func(struct xdp_md *ctx)
     //Start next header cursor position at data start
 	nh.pos = data;
 
-    if( (listen_dst_mac[0] == (__u8)*data)
+    if( (listen_dst_mac[0] == *((__u8*)data))
                 && (listen_dst_mac[1] == *((__u8*)data + 1))
                 && (listen_dst_mac[2] == *((__u8*)data + 2))
                 && (listen_dst_mac[3] == *((__u8*)data + 3))
