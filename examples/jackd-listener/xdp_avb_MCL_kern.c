60d68e8e60e30c2bc58ed120367db7960d5264df
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-12-14 11:33:22 +0100

    addd mcl kern.c

--- examples/jackd-listener/xdp_avb_MCL_kern.c
+++ examples/jackd-listener/xdp_avb_MCL_kern.c
@@ -113,14 +113,14 @@ int  xdp_avtp_func(struct xdp_md *ctx)
 	int nh_type = parse_ethhdr(&nh, data_end, &eth);
 	
     
-    if( nh_type == bpf_htons(ETH_P_TSN) ||  nh_type == ETH_P_TSN ){
+    if( (listen_dst_mac[0] == eth->h_dest[0])
+                && (listen_dst_mac[1] == eth->h_dest[1])
+                && (listen_dst_mac[2] == eth->h_dest[2])
+                && (listen_dst_mac[3] == eth->h_dest[3])
+                && (listen_dst_mac[4] == eth->h_dest[4])
+                && (listen_dst_mac[5] == eth->h_dest[5]) ){
+        if( nh_type == bpf_htons(ETH_P_TSN) ||  nh_type == ETH_P_TSN ){
             
-        if( (listen_dst_mac[0] == eth->h_dest[0])
-                    && (listen_dst_mac[1] == eth->h_dest[1])
-                    && (listen_dst_mac[2] == eth->h_dest[2])
-                    && (listen_dst_mac[3] == eth->h_dest[3])
-                    && (listen_dst_mac[4] == eth->h_dest[4])
-                    && (listen_dst_mac[5] == eth->h_dest[5]) ){
 
             seventeen22_header_t *hdr1722;
             __u8 proto1722 = parse_1722hdr(&nh, data_end, &hdr1722);
